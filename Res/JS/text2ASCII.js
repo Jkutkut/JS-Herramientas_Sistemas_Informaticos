60e9322d56909e54399fd0b7c49be48057c38dc2
jkutkut <jorgeregonzalez@gmail.com>
2021-09-30 16:07:56 +0200

    Code refactored and commented

--- Res/JS/text2ASCII.js
+++ Res/JS/text2ASCII.js
@@ -1,21 +1,35 @@
-function dec2bin(dec) {
-    return (dec >>> 0).toString(2);
-};
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-   
-readline.question('Name?', name => {
-    console.log(`Hey there ${name}!`);
+/**
+ * @param {String} text - String with the text you want to convert.
+ * @param {optional|boolean} log - Whenever you want extra log messages or no.
+ * @returns A string with all the binary numbers between spaces.
+ */
+function text2ASCII2binary(text, log=true) {
     let t = [];
-    for (let i = 0; i < name.length; i++) {
-        let code = name.charCodeAt(i);
-        let bin = dec2bin(code);
-        console.log(`${name[i]} -> ${code} -> ${bin}`);
+    for (let i = 0; i < text.length; i++) {
+        let code = text.charCodeAt(i);
+        let bin = (code >>> 0).toString(2);;
+        if (log) {
+            console.log(`${text[i]} -> ${code} -> ${bin}`);
+        }
         t.push(bin);
     }
-    readline.close();
-    console.log(t.join(" "));
-  });
+    return t.join(" ");
+}
+
+
+
+if (typeof require !== 'undefined' && require.main === module) {
+
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question(
+        "Enter the text: ",
+        text => {
+            console.log(text2ASCII2binary(text));
+            readline.close(); // Stop using the screen
+        }
+    );
+}
