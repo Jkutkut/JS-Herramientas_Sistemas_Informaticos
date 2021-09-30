9696e1987912bc69eb2b4350772006a7f2395163
= <=>
2021-10-01 00:18:52 +0200

    now number inputs can not be decimal nor less than 2. Converters now not using logs

--- sketch.js
+++ sketch.js
@@ -2,6 +2,16 @@ function baseConverterUpdate() {
     let number = document.getElementById("baseConverterInput").value;
     let baseFrom = document.getElementById("baseFrom").value;
     let baseTo = document.getElementById("baseTo").value;
+
+    document.getElementById("baseFrom").value = document.getElementById("baseFrom").value >>> 0;
+    if (baseFrom < 2) {
+        document.getElementById("baseFrom").value = 2;
+    }
+
+    document.getElementById("baseTo").value = document.getElementById("baseTo").value >>> 0;
+    if (baseTo < 2) {
+        document.getElementById("baseTo").value = 2;
+    }
     
     let output = baseConverter(number, baseFrom, baseTo);
     document.getElementById("baseConverterOutput").value = output;
@@ -11,7 +21,7 @@ function baseConverterUpdate() {
 function ascii2binConverter() {
     let content = document.getElementById("ascii2binaryInput").value;
 
-    let output = text2binaryConverter(content);
+    let output = text2binaryConverter(content, false);
 
     document.getElementById("ascii2binaryOutputCode").value = output.codes;
     document.getElementById("ascii2binaryOutputBin").value = output.output;
@@ -20,7 +30,7 @@ function ascii2binConverter() {
 function bin2asciiConverter() {
     let content = document.getElementById("binary2asciiInput").value;
 
-    let output = binary2textConverter(content);
+    let output = binary2textConverter(content, false);
 
     document.getElementById("binary2asciiOutputCode").value = output.codes;
     document.getElementById("binary2asciiOutputBin").value = output.output;
