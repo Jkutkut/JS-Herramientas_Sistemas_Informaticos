38187224d631ac1708e03c5637428b1bd41b178f
jkutkut <jorgeregonzalez@gmail.com>
2021-09-30 16:13:47 +0200

    code documented and refactored

--- Res/JS/ASCII2text.js
+++ Res/JS/ASCII2text.js
@@ -1,6 +1,9 @@
-function binary2ASCII(binArr) {
+/**
+ * @param {Array[String]} binArr - Array of strings with the binary representation of each character.
+ * @returns String Array with all the characters according to the ASCII code.
+ */
+function binary2ASCII2text(binArr) {
     var binCode = [];
-    
     for (i = 0; i < binArr.length; i++) {
         binCode.push(String.fromCharCode(parseInt(binArr[i], 2)));
     }
@@ -10,8 +13,6 @@ function binary2ASCII(binArr) {
 
 if (typeof require !== 'undefined' && require.main === module) {
     const input = '1001000 1100101 1101100 1101100 1101111 100000 1110111 1101111 1110010 1101100 1100100';
-    
-    let t = binary2ASCII(input.split(" "));
 
-    console.log(t.join(""));
+    console.log(binary2ASCII2text(input.split(" ")));
 }
