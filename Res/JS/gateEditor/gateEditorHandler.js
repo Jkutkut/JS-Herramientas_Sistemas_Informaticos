3aa53d4907504a5f8653c3af431ed051ba5a12cd
= <=>
2021-10-02 23:29:06 +0200

    This code shows the AND gate great

--- Res/JS/gateEditor/gateEditorHandler.js
+++ Res/JS/gateEditor/gateEditorHandler.js
@@ -9,39 +9,42 @@ window.onload = () => {
     gateEditorCanvas = document.getElementById('gateEditor');
     ctx = gateEditorCanvas.getContext('2d');
 
-    gates.push(new LogicGate(100, 100));
+    gates.push(new LogicGateAND(100, 100));
 
     show();
 }
 
 function show () {
+    ctx.save(); // save previous styles & set our current styles
+    
+    ctx.strokeStyle = 'blue'
+    ctx.fillStyle = 'blue'
+    ctx.lineWidth = 3
+
     for (let i = 0; i < gates.length; i++) {
         showElement(gates[i]);
     }
-}
-
-function showElement(element, filled=true) {
-    // save previous styles & set our current styles
-    ctx.save();
-    ctx.strokeStyle = 'blue'
-    ctx.fillStyle = 'blue'
-    ctx.lineWidth = 4
-
-    let shape = element.shape;
-
-    ctx.moveTo(...shape[0].pos) // sets our starting point
-    ctx.beginPath();
-    for (let i = 1; i < shape.length; i++) {
-        ctx.lineTo(...shape[i].pos);
-    }
-    if (filled) {
-        ctx.fill(); // no need to closePath, fill automatically closes the path
-        // ctx.stroke();
-    }
-    else {
-        ctx.closePath(); // left side and closes the path
-        ctx.stroke(); // draws it to screen via a stroke
-    }
 
     ctx.restore();
+}
+
+function showElement(element) {
+    let shape = element.shape;
+
+    for (let i = 0; i < shape.lines.length; i++) {
+        ctx.beginPath();
+        ctx.moveTo(...shape.lines[i][0].pos);
+        ctx.lineTo(...shape.lines[i][1].pos);
+        ctx.closePath();
+        ctx.stroke(); // update the screen
+
+    }
+    
+
+    for (let i = 0; i < shape.arcs.length; i++) {
+        ctx.beginPath();
+        ctx.arc(...shape.arcs[i]);
+        ctx.stroke();
+
+    }
 }
