bfc7709f6bb20a99636ada8b99c1033c933e673d
= <=>
2021-10-02 20:33:31 +0200

    basic shape of class more or less defined

--- Res/JS/gateEditor/logicGate.js
+++ Res/JS/gateEditor/logicGate.js
@@ -1,9 +1,20 @@
 class LogicGate {
     constructor (x, y) {
+        this._pos = new Point(x, y);
 
+        this._shape = [
+            new Point(-50, -50),
+            new Point(50, -50),
+            new Point(50, 50),
+            new Point(-50, 50)
+        ];
+    }
+
+    get pos() {
+        return this._pos;
     }
 
     get shape () {
-        return [];
+        return this._shape.map(x => x.times(this.pos));
     }
 }
