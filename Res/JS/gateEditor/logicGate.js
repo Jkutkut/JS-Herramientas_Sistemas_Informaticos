2855cea5db607a64c7509807b238808e0c8c1b9a
= <=>
2021-10-03 19:13:54 +0200

    Commented

--- Res/JS/gateEditor/logicGate.js
+++ Res/JS/gateEditor/logicGate.js
@@ -1,4 +1,7 @@
 // General object
+/**
+ * Prototype class to generate all the objects of the logicGateEditor.
+ */
 class LogicGateObject {
 
     static half_width = 1.8 * SHAPES_SIZE;
@@ -13,17 +16,25 @@ class LogicGateObject {
     }
 
     // Getters
-
+    /** 
+     * @returns Position of the center of the object as a Point instance.
+     */
     get pos() {
         return this._pos;
     }
 
+    /**
+     * Object with the lines and arcs needed to generate the object (both in arrays).
+     */
     get shape () {
         return this._shapeOBJ;
     }
 
-    isPointInside(p) {
-        
+    /**
+     * @param {Point} p - Point object
+     * @returns Whenever the coordinates p are inside/over the current object.
+     */
+    isPointInside(p) {    
         return (
             p.x >= this.pos.x - LogicGate.half_width &&
             p.x <= this.pos.x + LogicGate.half_width &&
@@ -32,97 +43,107 @@ class LogicGateObject {
         );
     }
 
+    /**
+     * Method to get the default shape of the object, centered at 0, 0
+     */
     get defaultShape() {
         return null;
     }
 
     // SETTERS
-
+    /**
+     * Moves the current object to the given coordinates.
+     * @param {number} x - New horizontal position.
+     * @param {number} y - New vertical position.
+     */
     moveTo(x, y) {
         this._pos.moveTo(x, y);
 
+        // Update object shape with the new coordinates
         this._shapeOBJ = {
             lines: [],
             arcs: []
         };
-
+        // Update lines
         for (let i = 0; i < this.defaultShape.lines.length; i++) {
             this._shapeOBJ.lines.push([]);
             for (let j = 0; j < 2; j++) {
                 this._shapeOBJ.lines[i].push(this.defaultShape.lines[i][j].plus(this.pos));
             }
         }
-
+        // Update arcs
         for (let i = 0; i < this.defaultShape.arcs.length; i++) {
             this._shapeOBJ.arcs.push([...this.defaultShape.arcs[i]]);
-
             for (let k = 0; k < 2; k++) {
                 this._shapeOBJ.arcs[i][k] = this._shapeOBJ.arcs[i][k] + this.pos.pos[k];
             }
         }
     }
 
+    /**
+     * Moves to the new coordinates.
+     * @param {Point} p - New coordinates as a Point instance.
+     */
     moveToPoint(p) {
         this.moveTo(...p.pos);
     }
 }
 
 // Logic gates
+
+/**
+ * Class that works as a prototype to generate all logic gates.
+ */
 class LogicGate extends LogicGateObject {
 
 }
 
 class LogicGateAND extends LogicGate {
-
     get defaultShape() {
         return SHAPES.AND;
     }
 }
 
 class LogicGateNAND extends LogicGate {
-
     get defaultShape() {
         return SHAPES.NAND;
     }
 }
 
 class LogicGateNOR extends LogicGate {
-
     get defaultShape() {
         return SHAPES.NOR;
     }
 }
 
 class LogicGateNOT extends LogicGate {
-
     get defaultShape() {
         return SHAPES.NOT;
     }
 }
 
 class LogicGateOR extends LogicGate {
-
     get defaultShape() {
         return SHAPES.OR;
     }
 }
 
 class LogicGateXNOR extends LogicGate {
-
     get defaultShape() {
         return SHAPES.XNOR;
     }
 }
 
 class LogicGateXOR extends LogicGate {
-
     get defaultShape() {
         return SHAPES.XOR;
     }
 }
 
-// input and output
-
+// Input and Output
+/**
+ * Prototype class to create the logic for the input and output classes.
+ */
 class LogicGateIO extends LogicGateObject {
     constructor(x, y, index) {
         super(x, y);
@@ -139,6 +160,10 @@ class LogicGateIO extends LogicGateObject {
         this.moveTo(x, y);
     }
 
+    /**
+     * The default shape stored only if defined. Else, return default value.
+     * @returns Object.
+     */
     get defaultShape() {
         if (this._defaultShape) {
             return this._defaultShape;
@@ -147,14 +172,26 @@ class LogicGateIO extends LogicGateObject {
         return {lines: [], arcs: []};
     }
 
+    /**
+     * The default shape of the object itself, without the name.
+     * @returns Object.
+     */
     get bodyShape() {
         return null;
     }
 
+    /**
+     * The name shape.
+     * @returns Object.
+     */
     get name() {
         return null;
     }
 
+    /**
+     * Return the index of the name selected.
+     * @returns The index stored only if defined. Else, return default value.
+     */
     get index() {
         if (this._index) {
             return this._index;
